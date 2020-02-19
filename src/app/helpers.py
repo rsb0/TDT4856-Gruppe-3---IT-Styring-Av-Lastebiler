7717b889bfce66da256d2e67244147eed90eca23
Mats Tyldum <ma.tyldum@gmail.com>
2020-02-19 13:37:08 +0100

    Update

--- src/app/helpers.py
+++ src/app/helpers.py
@@ -1,4 +1,22 @@
-import uuid
+import uuid, os
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
+
+# Key vault init
+key_vault_name = os.environ.get("KEY_VAULT_NAME")
+kv_uri = "https://" + key_vault_name + ".vault.azure.net"
+credential = DefaultAzureCredential()
+client = SecretClient(vault_url=kv_uri, credential=credential)
+secret_name_storage_key = os.environ.get("SECRET_NAME_STORAGE_KEY")
+secret_name_blob_string = os.environ.get("SECRET_NAME_BLOB_STRING")
+
+def get_table_key():
+    # return client.get_secret(secret_name_storage_key)
+    return ""
+    
+def get_blob_connection_string():
+    # return client.get_secret(secret_name_blob_string)
+    return ""
 
 def is_valid_uuid(val):
     try:
@@ -6,19 +24,3 @@ def is_valid_uuid(val):
         return True
     except ValueError:
         return False
-
-# TODO: Implement key vault helpers
-# Key vault init
-# key_vault_name = os.environ.get("KEY_VAULT_NAME")
-# KVUri = "https://" + key_vault_name + ".vault.azure.net"
-# credential = DefaultAzureCredential()
-# client = SecretClient(vault_url=KVUri, credential=credential)
-# secret_name = os.environ.get("SECRET_NAME")
-# retrieved_secret = client.get_secret(secret_name)
-
-def get_table_key():
-    return ""
-
-def get_blob_connection_string():
-    return ""
-    
--- src/app/input_handlers.py
+++ src/app/input_handlers.py
@@ -6,16 +6,16 @@ from helpers import get_table_key, get_blob_connection_string
 
 # Blob service init
 blob_service_client = BlobServiceClient.from_connection_string(get_blob_connection_string())
-#blob_container_name = os.environ.get("BLOB_CONTAINER_NAME")
+blob_container_name = os.environ.get("BLOB_CONTAINER_NAME")
 
 # Table service init
-#table_service = TableService(os.environ.get("DB_ACCOUNT_NAME"), account_key=get_table_key())
-#table_name = os.environ.get("DB_TABLE_NAME")
+table_service = TableService(os.environ.get("DB_ACCOUNT_NAME"), account_key=get_table_key())
+table_name = os.environ.get("DB_TABLE_NAME")
 
 # Uncomment for dev
-blob_container_name = os.environ.get("images")
-table_service = TableService("fuelpricestorage", account_key=get_table_key())
-table_name = "prices" 
+#blob_container_name = os.environ.get("images")
+#table_service = TableService("fuelpricestorage", account_key=get_table_key())
+#table_name = "prices" 
 
 def upload_picture_to_blob(image):
         # Create unambigous image file name
