6f49ed2a714476f03f4e19e52310e4d87580ed32
Sondre Olsen <sondrols@stud.ntnu.no>
2020-03-11 13:43:18 +0100

    First test run of pretrained resNet.

--- sign_detection/fuel_dataset.py
+++ sign_detection/fuel_dataset.py
@@ -1,11 +1,16 @@
 import os
 import torch
 import torch.utils.data
-from PIL import Image
+import torchvision
 from torchvision import transforms
+from engine import train_one_epoch, evaluate
+from PIL import Image
 from pycocotools.coco import COCO
 
-from utils import get_transform, collate_fn
+from utils_custom import get_transform, collate_fn_custom, get_model_instance_detection
+
+data_dir = 'data/images'
+coco_dir = 'coco/output.json'
 
 
 class fuelDataset(torch.utils.data.Dataset):
@@ -30,7 +35,7 @@ class fuelDataset(torch.utils.data.Dataset):
 
         # Load image
         img_path = coco.loadImgs(img_id)[0]['file_name']
-        img = Image.open(os.path.join(self.root, img_path))
+        img = Image.open(os.path.join(self.root, img_path)).convert('RGB')
 
         num_objs = len(coco_annotation)
 
@@ -77,7 +82,73 @@ class fuelDataset(torch.utils.data.Dataset):
         return len(self.ids)
 
 
+def train_func():
+    # train on the GPU or on the CPU, if a GPU is not available
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    # our dataset has two classes only - background and person
+    num_classes = 2
+    # use our dataset and defined transformations
+    dataset = fuelDataset(
+        root='data/images', annotations='coco/output.json', transforms=get_transform(train=False)
+    )
+    dataset_test = fuelDataset(
+        root='data/images', annotations='coco/output.json', transforms=get_transform(train=False)
+    )
+
+    # print('Length ', len(dataset))
+
+    # split the dataset in train and test set
+    indices = torch.randperm(len(dataset)).tolist()
+    dataset = torch.utils.data.Subset(dataset, indices[:-5])
+    dataset_test = torch.utils.data.Subset(dataset_test, indices[-5:])
+
+    # define training and validation data loaders
+    data_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=2, shuffle=True, num_workers=4, collate_fn=collate_fn_custom
+    )
+
+    print('hyyyyyy')
+
+    data_loader_test = torch.utils.data.DataLoader(
+        dataset_test, batch_size=1, shuffle=True, num_workers=4, collate_fn=collate_fn_custom
+    )
+
+    print('haaaaaaa')
+
+    # get the model using our helper function
+    model = get_model_instance_detection(num_classes)
+
+    # move model to the right device
+    model.to(device)
+
+    # construct an optimizer
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params, lr=0.005,
+                                momentum=0.9, weight_decay=0.0005)
+    # and a learning rate scheduler
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
+                                                   step_size=3,
+                                                   gamma=0.1)
+
+    # let's train it for 10 epochs
+    num_epochs = 4
+
+    for epoch in range(num_epochs):
+        # train for one epoch, printing every 10 iterations
+        train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
+        # update the learning rate
+        lr_scheduler.step()
+        # evaluate on the test dataset
+        evaluate(model, data_loader_test, device=device)
+
+    print("That's it!")
+
+
 if __name__ == "__main__":
+
+    print('Ey ey!')
+    """
 
     data_dir = 'data/images'
     coco_dir = 'coco/output.json'
@@ -90,10 +161,10 @@ if __name__ == "__main__":
 
     print('Oh')
 
-    train_batch_size = 1
+    train_batch_size = 2
 
     my_dataloader = torch.utils.data.DataLoader(
-        my_dataset, batch_size=train_batch_size, shuffle=True, num_workers=4, collate_fn=collate_fn
+        my_dataset, batch_size=train_batch_size, shuffle=True, num_workers=4, collate_fn=collate_fn_custom
     )
 
     print('Hey!')
@@ -105,3 +176,30 @@ if __name__ == "__main__":
         imgs = list(img.to(device) for img in imgs)
         annotations = [{k: v.to(device) for k, v in t.items()} for t in annotations]
         print(annotations)
+
+    print('Hihihi')
+
+    # 2 classes; Only target class or background
+    num_classes = 2
+    num_epochs = 10
+    # model = get_model_instance_detection(num_classes)
+
+    # For training
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+    print('Has loaded model')
+
+    images,targets = next(iter(my_dataloader))
+    images = list(image for image in images)
+    targets = [{k: v for k, v in t.items()} for t in targets]
+    print('Has loaded data from dataloader.')
+
+    output = model(images,targets)   # Returns losses and detections
+    print('Has finished running forward pass.')
+
+    # For inference
+    model.eval()
+    print('Has evaluated.')
+    x = [torch.rand(3, 300, 400), torch.rand(3, 500, 400)]
+    predictions = model(x)           # Returns predictions
+    """
+    train_func()
---
+++ sign_detection/utils_custom.py
@@ -0,0 +1,32 @@
+import torch
+import torchvision
+from torchvision import transforms
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+
+
+def get_transform(train):
+    trans = []
+    trans.append(transforms.ToTensor())
+
+    if train:
+        # transforms.append(transforms.RandomHorizontalFlip(0.5))
+        pass
+    return transforms.Compose(trans)
+
+
+def collate_fn_custom(batch):
+    return tuple(zip(*batch))
+
+
+def get_model_instance_detection(num_classes):
+    # load an instance segmentation model pre-trained pre-trained on COCO
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+    # get number of input features for the classifier
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    # replace the pre-trained head with a new one
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    return model
+
+
+
